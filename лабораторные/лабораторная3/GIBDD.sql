c3d0bc5f26b3f8cc1a6f94da105cf5592c0c3f87
BigTatyanos <66073160+BigTatyanos@users.noreply.github.com>
2021-04-15 23:40:30 +0500

    Update and rename JIBDD.sql to GIBDD.sql

--- лабораторные/лабораторная3/GIBDD.sql
+++ лабораторные/лабораторная3/GIBDD.sql
@@ -62,10 +62,10 @@ GO
 INSERT INTO [KN301_Borodina].TableOfregions.regions
  (Region, Number_region)
  VALUES 
- (N'Свердловская область', 66)
- ,(N'Челябинская область', 74)
- ,(N'Республика Башкирия', 02)
- ,(N'Москва', 77)	
+ (N'РЎРІРµСЂРґР»РѕРІСЃРєР°СЏ РѕР±Р»Р°СЃС‚СЊ', 66)
+ ,(N'Р§РµР»СЏР±РёРЅСЃРєР°СЏ РѕР±Р»Р°СЃС‚СЊ', 74)
+ ,(N'Р РµСЃРїСѓР±Р»РёРєР° Р‘Р°С€РєРёСЂРёСЏ', 02)
+ ,(N'РњРѕСЃРєРІР°', 77)	
 GO
 
 --SELECT * From [KN301_Borodina].TableOfregions.regions
@@ -127,9 +127,9 @@ IF NOT EXISTS
 	BEGIN
 		IF NOT EXISTS
 		(SELECT * FROM inserted
-		WHERE UPPER(SUBSTRING(Number_auto, 1, 1)) LIKE N'%[ABEKMHOPCTYXАВЕКМНОРСТУХ]%' 
+		WHERE UPPER(SUBSTRING(Number_auto, 1, 1)) LIKE N'%[ABEKMHOPCTYXРђР’Р•РљРњРќРћР РЎРўРЈРҐ]%' 
 			AND SUBSTRING(Number_auto, 2, 3) LIKE '%[0-9][0-9][0-9]%' AND CONVERT(int,SUBSTRING(Number_auto, 2, 3)) > 0
-			AND UPPER(SUBSTRING(Number_auto, 5, 2)) LIKE N'%[ABEKMHOPCTYXАВЕКМНОРСТУХ][ABEKMHOPCTYXАВЕКМНОРСТУХ]%'
+			AND UPPER(SUBSTRING(Number_auto, 5, 2)) LIKE N'%[ABEKMHOPCTYXРђР’Р•РљРњРќРћР РЎРўРЈРҐ][ABEKMHOPCTYXРђР’Р•РљРњРќРћР РЎРўРЈРҐ]%'
 			AND ((LEN(Number_auto) = 8 
 		AND SUBSTRING(Number_auto, 7, 2) LIKE '%[0-9][0-9]%' 
 		AND CONVERT(int,SUBSTRING(Number_auto, 7, 2)) > 0)
@@ -138,7 +138,7 @@ IF NOT EXISTS
 		AND SUBSTRING(Number_auto, 8, 2) LIKE '%[0-9][0-9]%' 
 		AND CONVERT(int,SUBSTRING(Number_auto, 8, 2)) > 0)))
 		BEGIN
-		PRINT 'Некорректный формат номера'
+		PRINT 'РќРµРєРѕСЂСЂРµРєС‚РЅС‹Р№ С„РѕСЂРјР°С‚ РЅРѕРјРµСЂР°'
 		END
 
 		IF EXISTS
@@ -157,13 +157,13 @@ IF NOT EXISTS
 		FROM TableOfregions.numbers_of_regions))	
 		)
 		BEGIN
-		PRINT 'Такого регион нет в справочнике '
+		PRINT 'РўР°РєРѕРіРѕ СЂРµРіРёРѕРЅ РЅРµС‚ РІ СЃРїСЂР°РІРѕС‡РЅРёРєРµ '
 		END
 
 		INSERT INTO Registor_cars.zurnal SELECT * FROM inserted
-		WHERE UPPER(SUBSTRING(Number_auto, 1, 1)) LIKE N'%[ABEKMHOPCTYXАВЕКМНОРСТУХ]%' 
+		WHERE UPPER(SUBSTRING(Number_auto, 1, 1)) LIKE N'%[ABEKMHOPCTYXРђР’Р•РљРњРќРћР РЎРўРЈРҐ]%' 
 			AND SUBSTRING(Number_auto, 2, 3) LIKE '%[0-9][0-9][0-9]%' AND CONVERT(int,SUBSTRING(Number_auto, 2, 3)) > 0
-			AND UPPER(SUBSTRING(Number_auto, 5, 2)) LIKE N'%[ABEKMHOPCTYXАВЕКМНОРСТУХ][ABEKMHOPCTYXАВЕКМНОРСТУХ]%'
+			AND UPPER(SUBSTRING(Number_auto, 5, 2)) LIKE N'%[ABEKMHOPCTYXРђР’Р•РљРњРќРћР РЎРўРЈРҐ][ABEKMHOPCTYXРђР’Р•РљРњРќРћР РЎРўРЈРҐ]%'
 			AND ((LEN(Number_auto) = 8 
 		AND SUBSTRING(Number_auto, 7, 2) LIKE '%[0-9][0-9]%' 
 		AND CONVERT(int,SUBSTRING(Number_auto, 7, 2)) > 0
@@ -186,7 +186,7 @@ ELSE
 		WHERE inserted.Number_auto = Registor_cars.zurnal.Number_auto 
 		ORDER BY Registor_cars.zurnal.Time_Dvigenia DESC),inserted.Time_Dvigenia) <5))
 		BEGIN
-		PRINT 'Меньше 5 минут с последней записи'
+		PRINT 'РњРµРЅСЊС€Рµ 5 РјРёРЅСѓС‚ СЃ РїРѕСЃР»РµРґРЅРµР№ Р·Р°РїРёСЃРё'
 		END
 
 		IF EXISTS
@@ -196,7 +196,7 @@ ELSE
 		WHERE inserted.Number_auto = Registor_cars.zurnal.Number_auto 
 		ORDER BY Registor_cars.zurnal.Time_Dvigenia DESC) = inserted.Napravlenie_Dvigenia)
 		BEGIN
-		PRINT 'Неправильное направление движения'
+		PRINT 'РќРµРїСЂР°РІРёР»СЊРЅРѕРµ РЅР°РїСЂР°РІР»РµРЅРёРµ РґРІРёР¶РµРЅРёСЏ'
 		END
 
 		INSERT INTO Registor_cars.zurnal 
@@ -218,7 +218,7 @@ ELSE
 GO
 
 CREATE VIEW transit AS
-SELECT a.Number_auto AS 'Номер авто', a.Time_Dvigenia AS 'Время въезда', MIN(b.Time_Dvigenia) AS 'Время выезда' , 
+SELECT a.Number_auto AS 'РќРѕРјРµСЂ Р°РІС‚Рѕ', a.Time_Dvigenia AS 'Р’СЂРµРјСЏ РІСЉРµР·РґР°', MIN(b.Time_Dvigenia) AS 'Р’СЂРµРјСЏ РІС‹РµР·РґР°' , 
 (SELECT TableOfregions.regions.Region
 FROM TableOfregions.regions
 WHERE TableOfregions.regions.Number_region =
@@ -227,7 +227,7 @@ FROM TableOfregions.numbers_of_regions
 WHERE  (LEN(a.Number_auto) = 8 
 AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 2)) = TableOfregions.numbers_of_regions.Second_number_region)
 OR (LEN(a.Number_auto) = 9 
-AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Регион'
+AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Р РµРіРёРѕРЅ'
 FROM Registor_cars.zurnal a, Registor_cars.zurnal b
 WHERE a.Number_auto = b.Number_auto
 AND b.Time_Dvigenia = 
@@ -240,7 +240,7 @@ ORDER BY c.Time_Dvigenia)
 AND a.Napravlenie_Dvigenia = 1 
 AND b.Napravlenie_Dvigenia = 0 
 AND a.Post != b.Post
-AND'Свердловская область' !=
+AND'РЎРІРµСЂРґР»РѕРІСЃРєР°СЏ РѕР±Р»Р°СЃС‚СЊ' !=
 (SELECT TableOfregions.regions.Region
 FROM TableOfregions.regions
 WHERE TableOfregions.regions.Number_region =
@@ -254,7 +254,7 @@ GROUP BY a.Number_auto, a.Time_Dvigenia
 GO
 
 CREATE VIEW domashnie AS
-SELECT a.Number_auto AS 'Номер авто', a.Time_Dvigenia AS 'Время выезда', MIN(b.Time_Dvigenia) AS 'Время въезда', 
+SELECT a.Number_auto AS 'РќРѕРјРµСЂ Р°РІС‚Рѕ', a.Time_Dvigenia AS 'Р’СЂРµРјСЏ РІС‹РµР·РґР°', MIN(b.Time_Dvigenia) AS 'Р’СЂРµРјСЏ РІСЉРµР·РґР°', 
 (SELECT TableOfregions.regions.Region
 FROM TableOfregions.regions
 WHERE TableOfregions.regions.Number_region =
@@ -263,13 +263,13 @@ FROM TableOfregions.numbers_of_regions
 WHERE  (LEN(a.Number_auto) = 8 
 AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 2)) = TableOfregions.numbers_of_regions.Second_number_region)
 OR (LEN(a.Number_auto) = 9 
-AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Регион'
+AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Р РµРіРёРѕРЅ'
 FROM Registor_cars.zurnal a, Registor_cars.zurnal b
 WHERE a.Number_auto = b.Number_auto 
 AND a.Napravlenie_Dvigenia = 0 
 AND b.Napravlenie_Dvigenia = 1 
 AND (DATEDIFF(MINUTE, a.Time_Dvigenia, b.Time_Dvigenia) > 0)
-AND'Свердловская область' =
+AND'РЎРІРµСЂРґР»РѕРІСЃРєР°СЏ РѕР±Р»Р°СЃС‚СЊ' =
 (SELECT TableOfregions.regions.Region
 FROM TableOfregions.regions
 WHERE TableOfregions.regions.Number_region =
@@ -283,7 +283,7 @@ GROUP BY a.Number_auto, a.Time_Dvigenia
 GO
 
 CREATE VIEW inogorodnie AS
-SELECT a.Number_auto AS 'Номер авто', a.Time_Dvigenia AS 'Время въезда', MIN(b.Time_Dvigenia) AS 'Время выезда', 
+SELECT a.Number_auto AS 'РќРѕРјРµСЂ Р°РІС‚Рѕ', a.Time_Dvigenia AS 'Р’СЂРµРјСЏ РІСЉРµР·РґР°', MIN(b.Time_Dvigenia) AS 'Р’СЂРµРјСЏ РІС‹РµР·РґР°', 
 (SELECT TableOfregions.regions.Region
 FROM TableOfregions.regions
 WHERE TableOfregions.regions.Number_region =
@@ -292,7 +292,7 @@ FROM TableOfregions.numbers_of_regions
 WHERE  (LEN(a.Number_auto) = 8 
 AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 2)) = TableOfregions.numbers_of_regions.Second_number_region)
 OR (LEN(a.Number_auto) = 9 
-AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Регион'
+AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Р РµРіРёРѕРЅ'
 FROM Registor_cars.zurnal a, Registor_cars.zurnal b
 WHERE a.Number_auto = b.Number_auto 
 AND b.Time_Dvigenia = 
@@ -309,7 +309,7 @@ GROUP BY a.Number_auto, a.Time_Dvigenia
 GO
 
 CREATE VIEW prochie AS
-SELECT a.Number_auto AS 'Номер авто', a.Time_Dvigenia AS 'Время въезда', MIN(b.Time_Dvigenia) AS 'Время выезда', 
+SELECT a.Number_auto AS 'РќРѕРјРµСЂ Р°РІС‚Рѕ', a.Time_Dvigenia AS 'Р’СЂРµРјСЏ РІСЉРµР·РґР°', MIN(b.Time_Dvigenia) AS 'Р’СЂРµРјСЏ РІС‹РµР·РґР°', 
 (SELECT TableOfregions.regions.Region
 FROM TableOfregions.regions
 WHERE TableOfregions.regions.Number_region =
@@ -318,12 +318,12 @@ FROM TableOfregions.numbers_of_regions
 WHERE  (LEN(a.Number_auto) = 8 
 AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 2)) = TableOfregions.numbers_of_regions.Second_number_region)
 OR (LEN(a.Number_auto) = 9 
-AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Регион'
+AND CONVERT(int,SUBSTRING(a.Number_auto, 7, 3)) = TableOfregions.numbers_of_regions.Second_number_region))) AS 'Р РµРіРёРѕРЅ'
 FROM Registor_cars.zurnal a, Registor_cars.zurnal b
 WHERE a.Number_auto = b.Number_auto 
-AND a.Number_auto NOT IN (SELECT transit.[Номер авто] FROM transit)
-AND a.Number_auto NOT IN (SELECT domashnie.[Номер авто] FROM domashnie)
-AND a.Number_auto NOT IN (SELECT inogorodnie.[Номер авто] FROM inogorodnie)
+AND a.Number_auto NOT IN (SELECT transit.[РќРѕРјРµСЂ Р°РІС‚Рѕ] FROM transit)
+AND a.Number_auto NOT IN (SELECT domashnie.[РќРѕРјРµСЂ Р°РІС‚Рѕ] FROM domashnie)
+AND a.Number_auto NOT IN (SELECT inogorodnie.[РќРѕРјРµСЂ Р°РІС‚Рѕ] FROM inogorodnie)
 AND b.Napravlenie_Dvigenia = 0
 AND a.Napravlenie_Dvigenia = 1
 GROUP BY a.Number_auto, a.Time_Dvigenia
@@ -333,60 +333,60 @@ GO
 
 --INSERT INTO [KN301_Borodina].Registor_cars.zurnal
 -- VALUES 
--- -- состоит из 9 символов
---(N'Ы305HG166', 1, 0, '12:10:00') --первый символ в номере буква Ы (не латиница)
--- ,(N'1305HH166', 1, 0, '12:10:00') --первый символ в номере цифра
--- ,(N'KK75HH166', 2, 1, '12:10:00') --второй символ в номере не цифра
--- ,(N'A3H9HH166', 3, 0, '12:20:00') --третий символ в номере не цифра
--- ,(N'A39HHH166', 4, 1, '12:30:00') --четвертый символ в номере не цифра
--- ,(N'A3,5HH166', 1, 1, '12:40:00') --дробное число во второй секции
--- ,(N'A000HH166', 1, 1, '12:40:00') --число равное нулю во второй секции
--- ,(N'T305ЫH166', 2, 0, '12:50:00') --пятый символ в номере буква Ы (не латиница)
--- ,(N'T385HЫ166', 3, 0, '12:50:00') --шестой символ в номере буква Ы (не латиница)
--- ,(N'T3051H166', 2, 0, '12:50:00') --пятый символ в номере цифра
--- ,(N'T385H1166', 3, 0, '12:50:00') --шестой символ в номере цифра
--- ,(N'T385HHF66', 3, 0, '12:50:00') --седьмой символ не цифра
--- ,(N'T385HH366', 3, 0, '12:50:00') --седьмой символ НЕ цифра 1,2,7
--- ,(N'T385HK1H6', 3, 0, '12:50:00') --восьмой символ не цифра
--- ,(N'T385HK16H', 3, 0, '12:50:00') --девятый символ не цифра
--- ,(N'T385HK100', 3, 0, '12:50:00') --восьмой и девятый символ нули
--- ,(N'T385HK1,6', 3, 0, '12:50:00') --дробное число в четвертой секции
--- ,(N't305hk166', 3, 0, '12:50:00') --(валид) маленькими буквами
--- ,(N'T085HK166', 3, 0, '12:50:00') --(валид) единица седьмой символ
--- ,(N'T005HK250', 3, 0, '12:50:00') --(валид) два седьмой символ
--- ,(N'T080KK702', 3, 0, '12:50:00') --(валид) семь седьмой символ
--- ,(N'А001ТТ174', 3, 0, '12:50:00') -- (валид) кириллица
--- -- состоит из 8 символов
--- ,(N'Ы305HY66', 1, 0, '12:10:00') --первый символ в номере буква Ы (не латиница)
--- ,(N'1305HY66', 1, 0, '12:10:00') --первый символ в номере цифра
--- ,(N'TT75HY66', 2, 1, '12:10:00') --второй символ в номере не цифра
--- ,(N'A3T9HH66', 3, 0, '12:20:00') --третий символ в номере не цифра
--- ,(N'A39THY66', 4, 1, '12:30:00') --четвертый символ в номере не цифра
--- ,(N'A3,5HT66', 1, 1, '12:40:00') --дробное число во второй секции
--- ,(N'A000HH66', 1, 1, '12:40:00') --число равное нулю во второй секции
--- ,(N'T305ЫY66', 2, 0, '12:50:00') --пятый символ в номере буква Ы (не латиница)
--- ,(N'T385HЫ66', 3, 0, '12:50:00') --шестой символ в номере буква Ы (не латиница)
--- ,(N'T3051K66', 2, 0, '12:50:00') --пятый символ в номере цифра
--- ,(N'T385H166', 3, 0, '12:50:00') --шестой символ в номере цифра
--- ,(N'T385HKH6', 3, 0, '12:50:00') --седьмой символ не цифра
--- ,(N'T385HK6H', 3, 0, '12:50:00') --восьмой символ не цифра
--- ,(N'T385HK00', 3, 0, '12:50:00') --седьмой и восьмой символ нули
--- ,(N't305hy74', 3, 0, '12:50:00') --(валид) маленькими буквами
--- ,(N'у001уу02', 3, 0, '12:50:00') -- (валид) кириллица
--- -- состоит из неправильного количества символов
--- ,(N't305hk7', 3, 0, '12:50:00') --меньше восьми
--- ,(N'H000HH2009', 3, 0, '12:50:00') -- больше девяти
+-- -- СЃРѕСЃС‚РѕРёС‚ РёР· 9 СЃРёРјРІРѕР»РѕРІ
+--(N'Р«305HG166', 1, 0, '12:10:00') --РїРµСЂРІС‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ Р±СѓРєРІР° Р« (РЅРµ Р»Р°С‚РёРЅРёС†Р°)
+-- ,(N'1305HH166', 1, 0, '12:10:00') --РїРµСЂРІС‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ С†РёС„СЂР°
+-- ,(N'KK75HH166', 2, 1, '12:10:00') --РІС‚РѕСЂРѕР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ РЅРµ С†РёС„СЂР°
+-- ,(N'A3H9HH166', 3, 0, '12:20:00') --С‚СЂРµС‚РёР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ РЅРµ С†РёС„СЂР°
+-- ,(N'A39HHH166', 4, 1, '12:30:00') --С‡РµС‚РІРµСЂС‚С‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ РЅРµ С†РёС„СЂР°
+-- ,(N'A3,5HH166', 1, 1, '12:40:00') --РґСЂРѕР±РЅРѕРµ С‡РёСЃР»Рѕ РІРѕ РІС‚РѕСЂРѕР№ СЃРµРєС†РёРё
+-- ,(N'A000HH166', 1, 1, '12:40:00') --С‡РёСЃР»Рѕ СЂР°РІРЅРѕРµ РЅСѓР»СЋ РІРѕ РІС‚РѕСЂРѕР№ СЃРµРєС†РёРё
+-- ,(N'T305Р«H166', 2, 0, '12:50:00') --РїСЏС‚С‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ Р±СѓРєРІР° Р« (РЅРµ Р»Р°С‚РёРЅРёС†Р°)
+-- ,(N'T385HР«166', 3, 0, '12:50:00') --С€РµСЃС‚РѕР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ Р±СѓРєРІР° Р« (РЅРµ Р»Р°С‚РёРЅРёС†Р°)
+-- ,(N'T3051H166', 2, 0, '12:50:00') --РїСЏС‚С‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ С†РёС„СЂР°
+-- ,(N'T385H1166', 3, 0, '12:50:00') --С€РµСЃС‚РѕР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ С†РёС„СЂР°
+-- ,(N'T385HHF66', 3, 0, '12:50:00') --СЃРµРґСЊРјРѕР№ СЃРёРјРІРѕР» РЅРµ С†РёС„СЂР°
+-- ,(N'T385HH366', 3, 0, '12:50:00') --СЃРµРґСЊРјРѕР№ СЃРёРјРІРѕР» РќР• С†РёС„СЂР° 1,2,7
+-- ,(N'T385HK1H6', 3, 0, '12:50:00') --РІРѕСЃСЊРјРѕР№ СЃРёРјРІРѕР» РЅРµ С†РёС„СЂР°
+-- ,(N'T385HK16H', 3, 0, '12:50:00') --РґРµРІСЏС‚С‹Р№ СЃРёРјРІРѕР» РЅРµ С†РёС„СЂР°
+-- ,(N'T385HK100', 3, 0, '12:50:00') --РІРѕСЃСЊРјРѕР№ Рё РґРµРІСЏС‚С‹Р№ СЃРёРјРІРѕР» РЅСѓР»Рё
+-- ,(N'T385HK1,6', 3, 0, '12:50:00') --РґСЂРѕР±РЅРѕРµ С‡РёСЃР»Рѕ РІ С‡РµС‚РІРµСЂС‚РѕР№ СЃРµРєС†РёРё
+-- ,(N't305hk166', 3, 0, '12:50:00') --(РІР°Р»РёРґ) РјР°Р»РµРЅСЊРєРёРјРё Р±СѓРєРІР°РјРё
+-- ,(N'T085HK166', 3, 0, '12:50:00') --(РІР°Р»РёРґ) РµРґРёРЅРёС†Р° СЃРµРґСЊРјРѕР№ СЃРёРјРІРѕР»
+-- ,(N'T005HK250', 3, 0, '12:50:00') --(РІР°Р»РёРґ) РґРІР° СЃРµРґСЊРјРѕР№ СЃРёРјРІРѕР»
+-- ,(N'T080KK702', 3, 0, '12:50:00') --(РІР°Р»РёРґ) СЃРµРјСЊ СЃРµРґСЊРјРѕР№ СЃРёРјРІРѕР»
+-- ,(N'Рђ001РўРў174', 3, 0, '12:50:00') -- (РІР°Р»РёРґ) РєРёСЂРёР»Р»РёС†Р°
+-- -- СЃРѕСЃС‚РѕРёС‚ РёР· 8 СЃРёРјРІРѕР»РѕРІ
+-- ,(N'Р«305HY66', 1, 0, '12:10:00') --РїРµСЂРІС‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ Р±СѓРєРІР° Р« (РЅРµ Р»Р°С‚РёРЅРёС†Р°)
+-- ,(N'1305HY66', 1, 0, '12:10:00') --РїРµСЂРІС‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ С†РёС„СЂР°
+-- ,(N'TT75HY66', 2, 1, '12:10:00') --РІС‚РѕСЂРѕР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ РЅРµ С†РёС„СЂР°
+-- ,(N'A3T9HH66', 3, 0, '12:20:00') --С‚СЂРµС‚РёР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ РЅРµ С†РёС„СЂР°
+-- ,(N'A39THY66', 4, 1, '12:30:00') --С‡РµС‚РІРµСЂС‚С‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ РЅРµ С†РёС„СЂР°
+-- ,(N'A3,5HT66', 1, 1, '12:40:00') --РґСЂРѕР±РЅРѕРµ С‡РёСЃР»Рѕ РІРѕ РІС‚РѕСЂРѕР№ СЃРµРєС†РёРё
+-- ,(N'A000HH66', 1, 1, '12:40:00') --С‡РёСЃР»Рѕ СЂР°РІРЅРѕРµ РЅСѓР»СЋ РІРѕ РІС‚РѕСЂРѕР№ СЃРµРєС†РёРё
+-- ,(N'T305Р«Y66', 2, 0, '12:50:00') --РїСЏС‚С‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ Р±СѓРєРІР° Р« (РЅРµ Р»Р°С‚РёРЅРёС†Р°)
+-- ,(N'T385HР«66', 3, 0, '12:50:00') --С€РµСЃС‚РѕР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ Р±СѓРєРІР° Р« (РЅРµ Р»Р°С‚РёРЅРёС†Р°)
+-- ,(N'T3051K66', 2, 0, '12:50:00') --РїСЏС‚С‹Р№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ С†РёС„СЂР°
+-- ,(N'T385H166', 3, 0, '12:50:00') --С€РµСЃС‚РѕР№ СЃРёРјРІРѕР» РІ РЅРѕРјРµСЂРµ С†РёС„СЂР°
+-- ,(N'T385HKH6', 3, 0, '12:50:00') --СЃРµРґСЊРјРѕР№ СЃРёРјРІРѕР» РЅРµ С†РёС„СЂР°
+-- ,(N'T385HK6H', 3, 0, '12:50:00') --РІРѕСЃСЊРјРѕР№ СЃРёРјРІРѕР» РЅРµ С†РёС„СЂР°
+-- ,(N'T385HK00', 3, 0, '12:50:00') --СЃРµРґСЊРјРѕР№ Рё РІРѕСЃСЊРјРѕР№ СЃРёРјРІРѕР» РЅСѓР»Рё
+-- ,(N't305hy74', 3, 0, '12:50:00') --(РІР°Р»РёРґ) РјР°Р»РµРЅСЊРєРёРјРё Р±СѓРєРІР°РјРё
+-- ,(N'Сѓ001СѓСѓ02', 3, 0, '12:50:00') -- (РІР°Р»РёРґ) РєРёСЂРёР»Р»РёС†Р°
+-- -- СЃРѕСЃС‚РѕРёС‚ РёР· РЅРµРїСЂР°РІРёР»СЊРЅРѕРіРѕ РєРѕР»РёС‡РµСЃС‚РІР° СЃРёРјРІРѕР»РѕРІ
+-- ,(N't305hk7', 3, 0, '12:50:00') --РјРµРЅСЊС€Рµ РІРѕСЃСЊРјРё
+-- ,(N'H000HH2009', 3, 0, '12:50:00') -- Р±РѕР»СЊС€Рµ РґРµРІСЏС‚Рё
 --GO
 
 
---  выехал 0
--- въехал 1
+--  РІС‹РµС…Р°Р» 0
+-- РІСЉРµС…Р°Р» 1
 
 
---тройка неккоректных номеров
+--С‚СЂРѕР№РєР° РЅРµРєРєРѕСЂРµРєС‚РЅС‹С… РЅРѕРјРµСЂРѕРІ
 --INSERT INTO [KN301_Borodina].Registor_cars.zurnal
 -- VALUES 
--- (N'Ы305HG166', 1, 0, '12:10:00')  
+-- (N'Р«305HG166', 1, 0, '12:10:00')  
 --GO
 
 --INSERT INTO [KN301_Borodina].Registor_cars.zurnal
@@ -399,13 +399,13 @@ GO
 -- (N'T385HK6H', 3, 0, '12:50:00')  
 --GO
 
----- нет такого региона в справочнике
+---- РЅРµС‚ С‚Р°РєРѕРіРѕ СЂРµРіРёРѕРЅР° РІ СЃРїСЂР°РІРѕС‡РЅРёРєРµ
 --INSERT INTO [KN301_Borodina].Registor_cars.zurnal
 -- VALUES 
 -- (N'T385HK45', 3, 0, '12:50:00')  
 --GO
 
----- разница во времени меньше 5 минут
+---- СЂР°Р·РЅРёС†Р° РІРѕ РІСЂРµРјРµРЅРё РјРµРЅСЊС€Рµ 5 РјРёРЅСѓС‚
 --INSERT INTO [KN301_Borodina].Registor_cars.zurnal
 -- VALUES 
 -- (N'Y370TT196', 4, 1, '15:06:00')  
@@ -416,7 +416,7 @@ GO
 -- (N'Y370TT196', 4, 0, '15:08:00')  
 --GO
 
-----два раза выехал 
+----РґРІР° СЂР°Р·Р° РІС‹РµС…Р°Р» 
 --INSERT INTO [KN301_Borodina].Registor_cars.zurnal
 -- VALUES 
 -- (N'Y340TT96', 4, 0, '10:06:00')  
@@ -427,7 +427,7 @@ GO
 -- (N'Y340TT96', 4, 0, '10:30:00')  
 --GO
 
----- транзитный автомобиль
+---- С‚СЂР°РЅР·РёС‚РЅС‹Р№ Р°РІС‚РѕРјРѕР±РёР»СЊ
 INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  VALUES 
  (N't305hy74', 2, 1, '10:06:00')  
@@ -448,7 +448,7 @@ INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  (N'K385KK74', 4, 0, '14:51:00') --
 GO
 
---домашний автомобиль
+--РґРѕРјР°С€РЅРёР№ Р°РІС‚РѕРјРѕР±РёР»СЊ
 INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  VALUES 
  (N'K245BB196', 4, 0, '11:06:00')  
@@ -459,7 +459,7 @@ INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  (N'K245BB196', 3, 1, '19:06:00')  
 GO
 
---иногородий автомобиль
+--РёРЅРѕРіРѕСЂРѕРґРёР№ Р°РІС‚РѕРјРѕР±РёР»СЊ
 INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  VALUES
  (N'Y001YY02', 3, 1, '9:00:00') -- 
@@ -470,7 +470,7 @@ INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  (N'Y001YY02', 3, 0, '19:30:00') 
 GO
 
---прочие автомобили
+--РїСЂРѕС‡РёРµ Р°РІС‚РѕРјРѕР±РёР»Рё
 INSERT INTO [KN301_Borodina].Registor_cars.zurnal
  VALUES 
  (N'A020BC702', 2, 0, '18:26:00') 
